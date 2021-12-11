5db53a1d9aecd6cae807e0fbe516ce378f4e6c09
yzohre <zohre.yosefi1996@yahoo.com>
2021-12-11 14:57:35 +0330

    123

---
+++ src/musics.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    source: './musics/Selseleye_Moye_Doost.mp3',
+    photo: './musics/Selseleye_Moye_Doost.jpg',
+    title: 'Selseleye Moye Doost'
+  },
+  {
+    source: './musics/Asemane_Eshgh.mp3',
+    photo: './musics/Asemane_Eshgh.jpg',
+    title: 'Asemane Eshgh'
+  },
+  {
+    source: './musics/Ze_Man_Negaram.mp3',
+    photo: './musics/Ze_Man_Negaram.jpg',
+    title: 'Ze Man Negaram'
+  }
+];
